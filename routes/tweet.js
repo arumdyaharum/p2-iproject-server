f3ea8cbd2a15dfc9fc0226a87635f3d4645b24cf
arumdyaharum <arumdyaharum@gmail.com>
2022-01-25 20:04:50 +0700

    router delete tweets done

--- routes/tweet.js
+++ routes/tweet.js
@@ -6,5 +6,6 @@ const authorization = require("../middlewares/tweetAuthor.js")
 router.post('/:id', authentication, tweetController.postTweets)
 router.get('/:id', authentication, tweetController.getTweets)
 router.put('/:tweetId', authentication, authorization, tweetController.putTweets)
+router.delete('/:tweetId', authentication, authorization, tweetController.deleteTweets)
 
 module.exports = router
