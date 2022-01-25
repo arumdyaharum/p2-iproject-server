e9ba5ae5d9bf0a3b5908b7fb198680b261b199e6
arumdyaharum <arumdyaharum@gmail.com>
2022-01-25 17:58:58 +0700

    router post folders done

--- controllers/folderController.js
+++ controllers/folderController.js
@@ -1,5 +1,21 @@
-class Controller {
+const { Folder } = require("../models")
 
+class Controller {
+  static async postFolders(req, res, next) {
+    try {
+      const value = {
+        name: req.body.name,
+        userId: req.currentUser.id
+      }
+      const result = await Folder.create(value)
+      res.status(201).json({
+        id: result.id,
+        name: result.name
+      })
+    } catch(err) {
+      next(err)
+    }
+  }
 }
 
 module.exports = Controller
