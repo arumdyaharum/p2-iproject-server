6da24e5a43f83fe2ed39e2e6f233fb9cfa1b23b1
arumdyaharum <arumdyaharum@gmail.com>
2022-01-25 18:08:41 +0700

    router get folders done

--- controllers/folderController.js
+++ controllers/folderController.js
@@ -1,4 +1,4 @@
-const { Folder } = require("../models")
+const { Folder, Tweet } = require("../models")
 
 class Controller {
   static async postFolders(req, res, next) {
@@ -12,6 +12,24 @@ class Controller {
         id: result.id,
         name: result.name
       })
+    } catch(err) {
+      next(err)
+    }
+  }
+  
+  static async getFolders(req, res, next) {
+    try {
+      const result = await Folder.findAll({
+        where: {userId: req.currentUser.id},
+        include: [
+          {model: Tweet}
+        ]
+      })
+      result.map(val => {
+        val.Tweet = val.Tweet.length
+        return val
+      })
+      res.status(200).json(result)
     } catch(err) {
       next(err)
     }
