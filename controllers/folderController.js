9b3df985bc8c2f0969ea32ab5d02d03220f62dd3
arumdyaharum <arumdyaharum@gmail.com>
2022-01-26 10:16:52 +0700

    merge from dev/get-folders /

--- controllers/folderController.js
+++ controllers/folderController.js
@@ -16,9 +16,20 @@ class Controller {
   
   static async getFolders(req, res, next) {
     try {
-      let result = await Folder.findAll({
+      const { page, size } = req.query
+      const items = +size || 10 
+      const toPage = +page || 1
+
+      let value = {
         where: {userId: req.currentUser.id}
-      })
+      }
+      if(size) {
+        value.limit = items
+      }
+      if(page) {
+        value.offset = (toPage - 1) * items
+      }
+      const result = await Folder.findAll(value)
       res.status(200).json(result)
     } catch(err) {
       next(err)
