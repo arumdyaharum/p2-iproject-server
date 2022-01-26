0f9e995adbb8e7750ac241b1cdf1f2a351b171d7
arumdyaharum <arumdyaharum@gmail.com>
2022-01-26 10:11:10 +0700

    add pagination filter

--- controllers/folderController.js
+++ controllers/folderController.js
@@ -19,16 +19,20 @@ class Controller {
   
   static async getFolders(req, res, next) {
     try {
-      const result = await Folder.findAll({
-        where: {userId: req.currentUser.id},
-        include: [
-          {model: Tweet}
-        ]
-      })
-      result.map(val => {
-        val.Tweet = val.Tweet.length
-        return val
-      })
+      const { page, size } = req.query
+      const items = +size || 10 
+      const toPage = +page || 1
+
+      let value = {
+        where: {userId: req.currentUser.id}
+      }
+      if(size) {
+        value.limit = items
+      }
+      if(page) {
+        value.offset = (toPage - 1) * items
+      }
+      const result = await Folder.findAll(value)
       res.status(200).json(result)
     } catch(err) {
       next(err)
